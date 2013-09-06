1e849e66deea0002361eb9ee5b33e575943a1d31
Fede85 <f.vanzati@arduino.cc>
2013-09-06 15:38:07 +0200

    Bridge library to the 1.5 format

---
+++ examples/HttpClient/HttpClient.ino
@@ -0,0 +1,26 @@
+
+#include <Bridge.h>
+#include <HttpClient.h>
+
+void setup() {
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  Bridge.begin();
+  Serial.begin(9600);
+  while(!Serial);
+}
+
+void loop() {
+  HttpClient client;
+  client.get("http://arduino.cc/asciilogo.txt");
+  
+  while (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
+  Serial.flush();
+  
+  delay(5000);
+}
+
+
